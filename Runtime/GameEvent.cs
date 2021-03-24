f0dd718468cd4848f6e29c58f6ab2730c0539841
Markus Baars <mbaars@web.de>
2021-03-24 11:41:36 +0100

    Initial commit

---
+++ Runtime/GameEvent.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MarkyabaGames.Packages.EventSystem
+{
+    /// <summary>
+    /// This scriptable object class creates a new asset file to manage custom events.
+    /// </summary>
+    [CreateAssetMenu(fileName = "NewGameEvent", menuName = "Game Event")]
+    public class GameEvent : ScriptableObject
+    {
+	    private readonly List<GameEventListener> m_Listeners = new List<GameEventListener>();
+
+        public void Raise()
+        {
+            for (int i = m_Listeners.Count; i >= 0; i--)
+            {
+                m_Listeners[i].OnEventRaised();
+            }
+        }
+
+        /// <summary>
+        /// Register the listener to the game event.
+        /// </summary>
+        /// <param name="listener">The <see cref="GameEventListener"/> to register.</param>
+        public void RegisterListener(GameEventListener listener)
+        {
+            if (!m_Listeners.Contains(listener))
+            {
+                m_Listeners.Add(listener);
+                return;
+            }
+
+            Debug.LogWarning($"The game event listener {listener} is already registered to the game event." );
+        }
+
+        /// <summary>
+        /// Unregister the listener out of the game event.
+        /// </summary>
+        /// <param name="listener">The <see cref="GameEventListener"/> to unregister.</param>
+        public void UnregisterListener(GameEventListener listener)
+        {
+            if (m_Listeners.Contains(listener))
+            {
+                m_Listeners.Remove(listener);
+                return;
+            }
+
+            Debug.LogWarning($"The game event listener {listener} is not registered to the game event.");
+        }
+    }
+}
---
+++ Runtime/GameEventListener.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace MarkyabaGames.Packages.EventSystem
+{
+    /// <summary>
+    /// The listener class that listens to an event raised by <see cref="GameEvent"/> object.
+    /// </summary>
+    public class GameEventListener : MonoBehaviour
+    {
+        [SerializeField] private GameEvent m_Event;
+        [SerializeField] private UnityEvent m_Response;
+
+        private void OnEnable()
+        {
+            m_Event.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            m_Event.UnregisterListener(this);
+        }
+
+        /// <summary>
+        /// Invokes all events added to the unity response event.
+        /// </summary>
+        public void OnEventRaised()
+        {
+            m_Response?.Invoke();
+        }
+    }
+}
